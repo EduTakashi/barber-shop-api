14dc94aefc02655f3e16460de84f76f6e79d8757
Eduardo Takashi Missaka <10417877@mackenzista.com.br>
2025-03-22 23:06:44 -0300

    BarberShopExceptionHandler updated

--- src/main/java/br/dio/barber_shop_ui/exceptionhandler/BarberShopExceptionHandler.java
+++ src/main/java/br/dio/barber_shop_ui/exceptionhandler/BarberShopExceptionHandler.java
@@ -3,28 +3,37 @@ package br.dio.barber_shop_ui.exceptionhandler;
 import br.dio.barber_shop_ui.controller.response.ProblemResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import java.time.OffsetDateTime;
-
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Log4j2
 @ControllerAdvice
 public class BarberShopExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(final RuntimeException ex, final WebRequest request) {
+        return buildErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleUncaught(final Exception ex, final WebRequest request){
-        log.error("handleUncaught: ", ex);
-        var status = INTERNAL_SERVER_ERROR;
+    public ResponseEntity<Object> handleUncaught(final Exception ex, final WebRequest request) {
+        return buildErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception ex, HttpStatus status, WebRequest request) {
+        log.error("Exception caught: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
+
         var response = ProblemResponse.builder()
                 .status(status.value())
                 .timestamp(OffsetDateTime.now())
                 .message(ex.getMessage())
                 .build();
-        return handleExceptionInternal(ex,response,new HttpHeaders(),status,request);
+
+        return handleExceptionInternal(ex, response, new HttpHeaders(), status, request);
     }
 }
